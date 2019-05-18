19be9172c8faeb31f9d9cb8b9bbff4b12590af81
Stephen Jacobs <stephenjacobs@gmail.com>
2019-05-18 19:01:42 -0400

    added redux-starter-kit and react-redux

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This is a base React Native template utilizing
 
 - Typescript
+- Redux-Starter-Kit
 - Styled-Components
 - React-Navigation
 - React-Native-Paper
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DarkTheme } from 'react-native-paper';
+
+export default {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: '#414854',
+    accent: '#FF6700',
+    background: '#DCDCDC',
+    surface: '#FFFFFF',
+    text: '#414854'
+  }
+};
